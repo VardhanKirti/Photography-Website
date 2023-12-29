8f5f4506a799007533e49989d3109e125b1893a8
Vardhan_Kirti <114055813+VardhanKirti@users.noreply.github.com>
2023-12-29 17:23:36 +0530

    Update script.js

--- photography website/script.js
+++ photography website/script.js
@@ -1,14 +1,1 @@
 
-        // External JavaScript code in your-script.js
-        document.addEventListener("DOMContentLoaded", function () {
-            // Function to change the navigation bar background color
-            function changeNavbarColor() {
-                var navBar = document.querySelector('.nav');
-                navBar.style.backgroundColor = 'lightcoral';
-            }
-
-            // Event listener for the "Explore Now" button
-            var exploreButton = document.querySelector('.explore_btn');
-            exploreButton.addEventListener('click', changeNavbarColor);
-        });
-   
